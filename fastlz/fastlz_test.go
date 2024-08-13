8a201922dd1cab0ddf046905ec98b9c3a2c8fc66
Kim <sjc02183@naver.com>
2024-08-13 15:33:51 +0200

    .

--- fastlz/fastlz_test.go
+++ fastlz/fastlz_test.go
@@ -1,1 +1,45 @@
 package fastlz
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestCompress(t *testing.T) {
+	bt := make([]byte, 10)
+	enc, err := Compress(bt)
+	require.NoError(t, err)
+
+	fmt.Println(enc)
+}
+
+func BenchmarkCompress(b *testing.B) {
+	b.Run("Length 2<<8", func(b *testing.B) {
+		bt := make([]byte, 2<<8)
+		for i := 0; i < b.N; i++ {
+			_, err := Compress(bt)
+			require.NoError(b, err)
+		}
+		b.SetBytes(int64(len(bt)))
+	})
+
+	b.Run("Length 2<<16", func(b *testing.B) {
+		bt := make([]byte, 2<<16)
+		for i := 0; i < b.N; i++ {
+			_, err := Compress(bt)
+			require.NoError(b, err)
+		}
+		b.SetBytes(int64(len(bt)))
+	})
+
+	b.Run("Length 2<<24", func(b *testing.B) {
+		bt := make([]byte, 2<<24)
+		for i := 0; i < b.N; i++ {
+			_, err := Compress(bt)
+			require.NoError(b, err)
+		}
+		b.SetBytes(int64(len(bt)))
+	})
+}
--- fastlzgo/fastlz.go
+++ fastlzgo/fastlz.go
@@ -1,1 +1,38 @@
 package fastlzgo
+
+import (
+	"errors"
+)
+
+func Compress(input []byte) ([]byte, error) {
+	length := len(input)
+	if length == 0 {
+		return nil, errors.New("no input provided")
+	}
+
+	result := make([]byte, length*2)
+	size := fastlzCompress(input, result)
+
+	if size == 0 {
+		return nil, errors.New("error compressing data")
+	}
+
+	return result[:size], nil
+}
+
+func fastlzCompress(input, output []byte) int {
+	if len(input) < 65536 {
+		return fastlz1Compress(input, output)
+	}
+	return fastlz2Compress(input, output)
+}
+
+func fastlz1Compress(input, output []byte) int {
+
+	return 0
+}
+
+func fastlz2Compress(input, output []byte) int {
+
+	return 0
+}
