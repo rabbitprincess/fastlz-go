25c4c280d8e635dfe6b5e8e6512b7e2b3530b85e
rabbitprincess <sjc02183@naver.com>
2024-08-14 16:40:48 +0900

    add benchmark

--- main_test.go
+++ main_test.go
@@ -18,3 +18,59 @@ func TestEncodeDecode(t *testing.T) {
 	require.NoError(t, err)
 	require.Equal(t, input, dec)
 }
+
+func BenchmarkCompress(b *testing.B) {
+	b.Run("fastlz cgo [Length 2<<8]", func(b *testing.B) {
+		bt := make([]byte, 2<<8)
+		for i := 0; i < b.N; i++ {
+			_, err := fastlz.Compress(bt)
+			require.NoError(b, err)
+		}
+		b.SetBytes(int64(len(bt)))
+	})
+
+	b.Run("fastlz  go [Length 2<<8]", func(b *testing.B) {
+		bt := make([]byte, 2<<8)
+		for i := 0; i < b.N; i++ {
+			_, err := fastlzgo.Compress(bt)
+			require.NoError(b, err)
+		}
+		b.SetBytes(int64(len(bt)))
+	})
+
+	b.Run("fastlz cgo [Length 2<<16]", func(b *testing.B) {
+		bt := make([]byte, 2<<16)
+		for i := 0; i < b.N; i++ {
+			_, err := fastlz.Compress(bt)
+			require.NoError(b, err)
+		}
+		b.SetBytes(int64(len(bt)))
+	})
+
+	b.Run("fastlz  go [Length 2<<16]", func(b *testing.B) {
+		bt := make([]byte, 2<<16)
+		for i := 0; i < b.N; i++ {
+			_, err := fastlzgo.Compress(bt)
+			require.NoError(b, err)
+		}
+		b.SetBytes(int64(len(bt)))
+	})
+
+	b.Run("fastlz cgo [Length 2<<24]", func(b *testing.B) {
+		bt := make([]byte, 2<<24)
+		for i := 0; i < b.N; i++ {
+			_, err := fastlz.Compress(bt)
+			require.NoError(b, err)
+		}
+		b.SetBytes(int64(len(bt)))
+	})
+
+	b.Run("fastlz  go [Length 2<<24]", func(b *testing.B) {
+		bt := make([]byte, 2<<24)
+		for i := 0; i < b.N; i++ {
+			_, err := fastlzgo.Compress(bt)
+			require.NoError(b, err)
+		}
+		b.SetBytes(int64(len(bt)))
+	})
+}
